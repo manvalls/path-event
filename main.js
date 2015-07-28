4905551738705cd1e94a9c0069ee1eb9710f7ade
Manuel Valls Fernández <manolo@vlrz.es>
2015-07-28 11:56:08 +0200

    Added pathEvent.prev()

--- main.js
+++ main.js
@@ -1,42 +1,72 @@
 var remainingPath = Symbol(),
+    pathname = Symbol(),
     emitter = Symbol();
 
 
 function PathEvent(path,e){
-  
-  this[remainingPath] = path;
-  this.rest = [];
-  
+  this[pathname] = path;
   this[emitter] = e;
-  
+
+  this.rest = [];
 }
 
 PathEvent.prototype.next = function(){
   var e = this[emitter],
-      name,part;
-  
-  if(!this.rest.length){
-    name = this[remainingPath];
-    
-    if(e.target.listeners(name)){
-      e.give(name,this);
-      return;
-    }
-    
+      name;
+
+  if(!this[remainingPath]){
+
+    name = this[pathname];
     this[remainingPath] = name.split('/');
-  }
-  
-  while(this[remainingPath].length > 1){
-    this.rest.push(this[remainingPath].pop());
-    name = this[remainingPath].concat('*').join('/');
-    
+
     if(e.target.listeners(name)){
       e.give(name,this);
       return;
     }
-    
+
   }
-  
+
+  while(this[remainingPath].length > 1){
+
+    this.rest.unshift(this[remainingPath].pop());
+    name = this[remainingPath].concat('*').join('/');
+
+    if(e.target.listeners(name)){
+      e.give(name,this);
+      return;
+    }
+
+  }
+
+};
+
+PathEvent.prototype.prev = function(){
+  var e = this[emitter],
+      name;
+
+  while(this.rest.length){
+    this[remainingPath].push(this.rest.shift());
+    name = this[remainingPath].concat('*').join('/');
+
+    if(e.target.listeners(name)){
+      e.give(name,this);
+      return;
+    }
+
+  }
+
+  if(this[remainingPath]){
+
+    name = this[pathname];
+    this[remainingPath] = null;
+
+    if(e.target.listeners(name)){
+      e.give(name,this);
+      return;
+    }
+
+  }
+
 };
 
 module.exports = PathEvent;

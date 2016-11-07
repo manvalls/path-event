18b7cf8fc3df19275f544098b0b441982fb02f76
Manuel Valls Fernández <manolo@vlrz.es>
2016-11-07 20:24:13 +0100

    Now using ES2015 classes

--- main.js
+++ main.js
@@ -3,67 +3,64 @@ var Lock = require('y-lock'),
     define = require('u-proto/define'),
     pct = require('pct'),
 
-    lock = Symbol(),
     common = Symbol();
 
 // PathEvent
 
-function PathEvent(path,e,max,prefixes){
-  var remaining,rest,prefix,args,joined;
+class PathEvent extends Lock{
 
-  prefixes = prefixes || [];
-  prefixes.push('');
+  constructor(path,e,max,prefixes){
+    var remaining,rest,prefix,args,joined;
 
-  Lock.call(this,0);
-  this[common] = this;
+    prefixes = prefixes || [];
+    prefixes.push('');
 
-  for(prefix of prefixes) e.give(prefix + path,Object.create(this,{
-    args: {value: ''}
-  }));
-
-  if(max != null){
-
-    if(!max){
-      remaining = [];
-      rest = [path];
-    }else{
-      remaining = path.split('/',max + 1);
-      joined = remaining.join('/');
-
-      if(joined == path) rest = [];
-      else rest = [path.replace(joined + '/','')];
-    }
-
-  }else{
-    remaining = path.split('/');
-    rest = [];
-  }
-
-  while(remaining.length > 1){
-    rest.unshift(remaining.pop());
-    path = remaining.concat('*').join('/');
-    args = rest.join('/');
+    super(0);
+    this[common] = this;
 
     for(prefix of prefixes) e.give(prefix + path,Object.create(this,{
-      args: {value: args}
+      args: {value: ''}
     }));
+
+    if(max != null){
+
+      if(!max){
+        remaining = [];
+        rest = [path];
+      }else{
+        remaining = path.split('/',max + 1);
+        joined = remaining.join('/');
+
+        if(joined == path) rest = [];
+        else rest = [path.replace(joined + '/','')];
+      }
+
+    }else{
+      remaining = path.split('/');
+      rest = [];
+    }
+
+    while(remaining.length > 1){
+      rest.unshift(remaining.pop());
+      path = remaining.concat('*').join('/');
+      args = rest.join('/');
+
+      for(prefix of prefixes) e.give(prefix + path,Object.create(this,{
+        args: {value: args}
+      }));
+
+    }
 
   }
 
-}
+  get common(){ return this[common]; }
 
-PathEvent.prototype = Object.create(Lock.prototype);
-PathEvent.prototype[define]('constructor',PathEvent);
-PathEvent.prototype[define]({
-
-  get common(){ return this[common]; },
-
-  argv: function(n){
+  argv(n){
     if(n != null) return this.args.split('/',n).map(decode);
     return this.args.split('/').map(decode);
   }
 
-});
+}
 
 // - utils
 
